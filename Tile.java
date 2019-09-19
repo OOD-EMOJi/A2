40607cd167934ac5cca405d0c2dfb3009905be94
ofulcher16 <olympia.fulcher16@ncf.edu>
2019-09-19 19:41:23 -0400

    Tile.java class
    Class for the Tile object, isWall is encapsulated so that the boolean can't change.

---
+++ Tile.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mazeassignment.mavenproject2;
+
+/**
+ *
+ * @author olymp
+ */
+public class Tile {
+    private boolean isWall;
+
+    public Tile(boolean isWall) {
+        this.isWall = isWall;
+    }
+}
