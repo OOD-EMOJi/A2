56ee4d5c782e65141abbc8f852a7c84d2ae715e1
Ellie Young <43616100+elllie@users.noreply.github.com>
2019-09-21 10:14:33 -0400

    updated printer to check for isWall()

--- MazePrinter.java
+++ MazePrinter.java
@@ -8,7 +8,7 @@ public class MazePrinter {
         System.out.println();
         for (int i=0; i < maze.tiles.length; i++) {
             for (int j=0; j < maze.tiles[i].length; j++) {
-                if (maze.tiles[i][j].isWall)
+                if (maze.tiles[i][j].isWall())
                     System.out.print("█");
                 else
                     System.out.print(" ");
