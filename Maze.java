c0747125a51f7aa2b153ef3554e77cc879762f85
ofulcher16 <olympia.fulcher16@ncf.edu>
2019-09-22 13:09:26 -0400

    Maze.java
    Literally wrote this in 20 minutes so if it's fucked up let me know

---
+++ Maze.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author olymp
+ */
+public class Maze {
+    Tile [][] tiles;
+    MazeGenerator generator;
+
+    public Maze(Tile[][] tiles, MazeGenerator generator) {
+        this.tiles = tiles;
+        this.generator = generator;
+    }
+
+    void generateMaze(){
+        for (Tile[] a : tiles){
+            for(Tile b : a){
+                b.setIsWall(true);
+            }
+        }
+    }
+    
+}
